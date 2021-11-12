20f29d58704177ecf18a8a4b4a52bf498c572306
Wilmord <berkayelbir10@hotmail.com>
2021-11-12 22:05:33 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	fmt.Println("---My gitlab automotor---")
-	gitlabProj := newProject(401, "https://gitlab.com/")
+	gitlabProj := newProject(projectID, "https://gitlab.com/")
 	label, err := gitlabProj.findLabel("label-name")
 	if err != nil {
 		fmt.Println(err.Error())
