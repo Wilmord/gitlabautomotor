f8e42b4eacb1f30b4e7fb4b6671321d6ec619561
Wilmord <berkayelbir10@hotmail.com>
2021-11-12 22:04:26 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,13 +6,8 @@ import (
 
 func main() {
 	fmt.Println("---My gitlab automotor---")
-<<<<<<< HEAD
-	gitlabProj := newProject(401, "https://bro-gitlab.w2k.feico.com/")
-	label, err := gitlabProj.findLabel("ccb-pending")
-=======
 	gitlabProj := newProject(401, "https://gitlab.com/")
 	label, err := gitlabProj.findLabel("label-name")
->>>>>>> b5bc889 (first commit)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
